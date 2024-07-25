f5784ffc275f4233b894666a6e7028a3b4f7ca9f
Anthony Zurita <anthonyy.zurita@gmail.com>
2024-07-25 10:11:59 -0400

    In this commit the following changes were made:  -- The logo text in the nav bar is now a link to     the home page.  -- Configured Next.config.js to be able to use     external link images in the project.  -- Started making the homepage by adding     containers to help layout the text and images     that will be on the page.  -- Started a home.module.css file to style the     homepage. Realized that the bug with the     styling sheet not applying was because I     misspelled the file name when creating it. It     should have been home.module.css instead of     home.modules.css.

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+import styles from "./about.module.css";
+
 const AboutPage = () => {
-    return (
-        <div>AboutPage</div>
-    )
-}
+  return (
+    <div className={styles.imgContainer}>
+      <div>
+        <Image src="https://images.pexels.com/photos/27108021/pexels-photo-27108021/free-photo-of-a-building-with-a-red-and-white-clock-on-it.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" fill />
+      </div>
+    </div>
+  );
+};
 
 export default AboutPage;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,29 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
 const Home = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className={styles.container}>
+      Homepage
+      <div className={styles.textContainer}>
+        <h1>Creative Thoughts Agency.</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consecutur adipisicing elit. Vero
+          blanditiis, voluptates, quod, quae voluptatum quibusdam quial.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandsImg} />
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
